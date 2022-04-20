64c9af7c42897190eb9860212d83ec33647c39c3
Claudio Lopes <claudio.lopes@webjump.co.br>
2022-04-20 16:08:34 -0300

    report

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
+source 'https://rubygems.org'
 gem 'capybara'
-gem 'cucumber'
+gem 'cucumber', '~> 3.2'
+gem 'faker'
+gem 'ffi'
+gem 'report_builder'
 gem 'rspec'
+gem 'rufo'
 gem 'selenium-webdriver'
-
---
+++ features/support/hooks.rb
@@ -0,0 +1,29 @@
+require "cucumber/rake/task"
+require 'erb'
+After do |scenario|
+    Dir.mkdir("Data") unless Dir.exist?("Data")
+    Dir.mkdir("Data/Reports") unless Dir.exist?("Data/Reports")
+    sufix = ("error" if scenario.failed?) || "success"
+    name = scenario.name.tr(" ", "_").downcase
+    image_name = "Data/Reports/img/#{sufix}-#{name}.png"
+    temp_shot = page.save_screenshot(image_name)
+    file_shot = File.open(temp_shot, "rb").read
+    final_shot = Base64.encode64(file_shot)
+    embed(temp_shot, "image/png", "Evidência!")
+end
+at_exit do
+ReportBuilder.configure do |config|
+    config.json_path = 'Data/Reports/report.json'
+    config.report_path = 'Data/Reports/teste regressivo'
+    config.report_types = [:html]
+    config.report_title = 'Meus Testes'
+    config.include_images = true
+    config.compress_images = false
+    config.color = "indigo"
+    config.additional_info = {browser: 'Chrome',
+                              environment: 'Teste Luma',
+                              QA: 'Claudio Lopes',
+                              Projeto: 'Report Luma'}
+  end
+ReportBuilder.build_report
+end
